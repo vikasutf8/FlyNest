4ff09e7c00d1128a4984321d8d32a1068ce1c82d
Vikas Arya <117626243+vikasutf8@users.noreply.github.com>
2026-04-18 13:30:30 +0530

    Add flight-ops-service module with initial setup and configuration

---
+++ services/flight-ops-service/src/main/java/com/flynest/flight_ops_service/FlightOpsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flynest.flight_ops_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightOpsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightOpsServiceApplication.class, args);
+	}
+
+}
---
+++ services/flight-ops-service/src/test/java/com/flynest/flight_ops_service/FlightOpsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flynest.flight_ops_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightOpsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
